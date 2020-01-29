47f072dbd3ade35942ebc95ffb8252d19d7251f7
zorn <zorn7@yandex.ru>
2020-01-29 23:34:55 +1000

    DI stuff

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ZornV\Symfony\MessengerSupervisorBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('messenger_supervisor');
+
+        $treeBuilder->getRootNode()
+            ->children()
+                ->normalizeKeys(false)
+                ->arrayPrototype()
+                    ->normalizeKeys(false)
+                    ->children()
+                        ->arrayNode('receivers')
+                            ->scalarPrototype()->end()
+                        ->end()
+                        ->scalarNode('memory-limit')->end()
+                        ->integerNode('time-limit')->end()
+                        ->integerNode('limit')->end()
+                        ->integerNode('sleep')->end()
+                        ->scalarNode('bus')->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
---
+++ src/DependencyInjection/MessengerSupervisorExtension.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace ZornV\Symfony\MessengerSupervisorBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
+
+class MessengerSupervisorExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+/* validation
+        $transports = array_keys($config['transports']);
+        $buses = array_keys($config['buses']);
+        foreach ($config['supervisor'] as $name => $consumer) {
+            foreach ($consumer['receivers'] as $receiver) {
+                if (!\in_array($receiver, $transports)) {
+                    throw new InvalidConfigurationException(sprintf('Invalid receiver "%s" in "%s" messenger consumer. Available transports are "%s"', $receiver, $name, implode('", "', $transports)));
+                }
+            }
+            if (isset($consumer['bus']) && !\in_array($consumer['bus'], $buses)) {
+                throw new InvalidConfigurationException(sprintf('Invalid bus "%s" in "%s" messenger consumer. Available buses are "%s"', $consumer['bus'], $name, implode('", "', $buses)));
+            }
+        }
+        $container->setParameter('messenger.supervisor', $config['supervisor']);
+*/
+        $configuration = new Configuration();
+
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new XmlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('services.xml');
+    }
+}
